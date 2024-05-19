45908a0e1ad2374065ee1ac348088c08275109a5
fprh13 <fprh13@gmail.com>
2024-05-19 23:43:16 +0900

    Feat: 글 상세 보기 기능

--- src/main/java/com/example/blog/controller/PostPageController.java
+++ src/main/java/com/example/blog/controller/PostPageController.java
@@ -1,6 +1,26 @@
 package com.example.blog.controller;
 
 
+import com.example.blog.service.PostService;
+import com.example.blog.vo.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PostPageController {
 
+    private final PostService postService;
+
+    public PostPageController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @RequestMapping("/post/{id}")
+    public String getPostDetailPage(Model model, @PathVariable Integer id) {
+        Post post = postService.getPostById(id);
+        model.addAttribute("post", post);
+        return "post_detail";
+    }
 }
